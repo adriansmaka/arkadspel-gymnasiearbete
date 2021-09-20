a74961c785e679ab7c495f07c140399be05ca405
Linuse1 <linus.eriksson@edu.nykoping.se>
2021-09-20 13:23:24 +0200

    fortsatte

--- game.py
+++ game.py
@@ -1,19 +1,35 @@
 import pygame
-from pygame.locals import *
+import os
 
 pygame.init()
+#width, height = 1280, 960
+width, height = 1100, 748
+fönster = pygame.display.set_mode((width, height))
+fps = 60
+vit = (255, 255, 255)
+karaktär = pygame.image.load(os.path.join('img', 'karaktär.png'))
+karaktär = pygame.transform.scale(karaktär, (95, 70))
+fiende = pygame.image.load(os.path.join('img', 'fiende_1.png'))
 
-screen_width = 640
-screen_height = 480
+def display_fönster():
+    fönster.fill(vit)
+    fönster.blit(karaktär, (470, 500))
+    pygame.display.update()
+    
 
-screen = pygame.display.set_mode((screen_width))
-pygame.display.set_caption("Flappy Bird")
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+        display_fönster()
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+pygame.QUIT
 
-pygame.quit()
+#if __name__ == "__main__":
 
+main()
