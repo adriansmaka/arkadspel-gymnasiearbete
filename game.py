5c3290576c1e2c9bfe11274ef16906fb18960761
Linuse1 <linus.eriksson@edu.nykoping.se>
2021-09-28 13:42:53 +0200

    1234567890

--- game.py
+++ game.py
@@ -1,48 +1,33 @@
-from typing import KeysView
 import pygame
-import os
+import sys
+from pygame.locals import *
 
 pygame.init()
-#width, height = 1280, 960
-width, height = 1100, 748
-window = pygame.display.set_mode((width, height))
-fps = 60
-hastighet = 5 
-vit = (255, 255, 255)
+vec = pygame.math.Vector2  # 2 for two dimensional
 
-karaktär_img = pygame.image.load(os.path.join('img', 'ship.png'))
-karaktär = pygame.transform.scale(karaktär_img, (50, 50))
+HEIGHT = 450
+WIDTH = 400
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
 
-fiende_img = pygame.image.load(os.path.join('img', 'enemy_1.png'))
-fiende = pygame.transform.scale(fiende_img, (87, 50))
+FramePerSec = pygame.time.Clock()
 
-def display_fönster(karaktär_spel, fiende_spel):
-    fönster.fill(vit)
-    fönster.blit(karaktär, (490, 500))
-    fönster.blit(fiende, (490, 200))
-    pygame.display.update()
-    
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-def main():
-    karaktär_spel = pygame.Rect((490, 500), (95, 70))
-    fiende_spel = pygame.Rect((490, 200), (87, 50))
+        elif event.type == pygame.KEYDOWN:            
+            # print key
+            print(event.key)
 
-    clock = pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(fps)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            
-        keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_a]:
-            karaktär_spel.x -= hastighet
-        
-        display_fönster(karaktär_spel, fiende_spel)
+            # print unicode representation
+            print(event.unicode)
 
-pygame.QUIT
+            # print user firendly name
+            print(pygame.key.name(event.key))
 
-#if __name__ == "__main__":
-
-main()
+    displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Game")
