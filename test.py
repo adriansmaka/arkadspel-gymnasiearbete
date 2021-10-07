4783d2cd1cdc2bd135353c8a82037bcfd0e6a9ec
adriansmaka <adrian.smaka@edu.nykoping.se>
2021-10-07 18:52:04 +0200

    Fixat border för char

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ width, height = 1100, 748
 heightdiv= height / 2
 window = pygame.display.set_mode((width, height))
 fps = 60
-velocity = 4
+velocity = 10
 white = (255, 255, 255)
 black = (0, 0, 0)
 
@@ -25,7 +25,7 @@ nme_img = pygame.image.load(os.path.join('img', 'enemy_1.png'))
 nme_w = 87
 nme_h = 50
 nme = pygame.transform.scale(nme_img, (nme_w, nme_h))
-nme_pos = 0, 10
+nme_x, nme_y = 0, 10
 
 def display_triangle():
     pygame.draw.polygon((window), (255, 255, 0), [(char_x, char_y+50), (char_x+25, char_y), (char_x+50, char_y+50)])
@@ -39,7 +39,8 @@ def display_window(char_game, nme_game):
     pygame.display.update()
 
 def char_movement(keys_pressed, char_game):
-        if keys_pressed[pygame.K_a] and char_game.x - velocity > 0:
+    #char movement
+        if keys_pressed[pygame.K_a]:
             char_game.x -= velocity
         if keys_pressed[pygame.K_d]:
             char_game.x += velocity
@@ -47,17 +48,27 @@ def char_movement(keys_pressed, char_game):
             char_game.y += velocity 
         if keys_pressed[pygame.K_w]:
             char_game.y -= velocity
+    #char border
+        if char_game.x < 0:
+            char_game.x = 0
+        if char_game.x > width-char_w:
+            char_game.x = width-char_w
+        if char_game.y > height-char_h:
+            char_game.y = height-char_h
+        if char_game.y < heightdiv-char_h:
+            char_game.y = heightdiv-char_h
 
-        if pygame.Rect.colliderect(char_game):
-            print("Crash!")
+
+            #do not delete
+            #-------------
+            #if pygame.Rect.colliderect(char_game, wall):
+                #print("Crash!")
+
 
 def main():
     #starting position, (x, y), (w, h)
     char_game = pygame.Rect((char_x, char_y), (char_w, char_h))
-    nme_game = pygame.Rect((nme_pos), (nme_w, nme_h))
-
-    if pygame.Rect.colliderect(wall):
-        print("Crash!")
+    nme_game = pygame.Rect((nme_x, nme_y), (nme_w, nme_h))
 
     #game run
     clock = pygame.time.Clock()
