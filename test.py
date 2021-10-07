2cff4dd9b171de64a6f5a833e93b1d0f333cd961
adriansmaka <adrian.smaka@edu.nykoping.se>
2021-10-07 19:03:28 +0200

    Ändrat utseendet på border

--- test.py
+++ test.py
@@ -5,15 +5,17 @@ import os
 pygame.init()
 #width, height = 1280, 960
 width, height = 1100, 748
-heightdiv= height / 2
+heightdiv = height / 2
+border_y = height * 0.4
 window = pygame.display.set_mode((width, height))
 fps = 60
 velocity = 10
 white = (255, 255, 255)
 black = (0, 0, 0)
+green = (0, 255, 0)
 
 # x-kord, Y-kord, tjocklek, höjd
-wall = pygame.Rect(0, 320, width, 5)
+wall = pygame.Rect(0, border_y, width, height)
 
 char_img = pygame.image.load(os.path.join('img', 'ship.png'))
 char_w, char_h = 50, 50
@@ -32,7 +34,7 @@ def display_triangle():
 
 def display_window(char_game, nme_game):
     window.fill(white)
-    pygame.draw.rect(window, black, wall)
+    pygame.draw.rect(window, green, wall)
     window.blit(char, (char_game.x, char_game.y))
     window.blit(nme, (nme_game.x, nme_game.y))
     display_triangle()
@@ -55,8 +57,8 @@ def char_movement(keys_pressed, char_game):
             char_game.x = width-char_w
         if char_game.y > height-char_h:
             char_game.y = height-char_h
-        if char_game.y < heightdiv-char_h:
-            char_game.y = heightdiv-char_h
+        if char_game.y < border_y:
+            char_game.y = border_y
 
 
             #do not delete
