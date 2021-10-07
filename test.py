8c59336207dde23b66679e0e14ab45c525efb25e
ElektriikAtk <adrian.smaka@edu.nykoping.se>
2021-10-07 13:16:26 +0200

    testpush igen

--- test.py
+++ test.py
@@ -16,11 +16,10 @@ black = (0, 0, 0)
 wall = pygame.Rect(0, 320, width, 5)
 
 char_img = pygame.image.load(os.path.join('img', 'ship.png'))
-char_w = 50
-char_h = 50
+char_w, char_h = 50, 50
+
 char = pygame.transform.scale(char_img, (char_w, char_h))
-char_x = 490
-char_y = 500
+char_x, char_y = 0, 0
 
 nme_img = pygame.image.load(os.path.join('img', 'enemy_1.png'))
 nme_w = 87
@@ -28,14 +27,15 @@ nme_h = 50
 nme = pygame.transform.scale(nme_img, (nme_w, nme_h))
 nme_pos = 0, 10
 
-triangle = pygame.draw.polygon((window), (255, 255, 0), [(char_x, char_y), (char_x+25, char_y+25), (char_x+50, char_y-50)])
+def display_triangle():
+    pygame.draw.polygon((window), (255, 255, 0), [(char_x, char_y+50), (char_x+25, char_y), (char_x+50, char_y+50)])
 
-def display_window(char_game, nme_game, triangle):
+def display_window(char_game, nme_game):
     window.fill(white)
     pygame.draw.rect(window, black, wall)
     window.blit(char, (char_game.x, char_game.y))
     window.blit(nme, (nme_game.x, nme_game.y))
-    window.blit(triangle, (char_game.x, char_game.y))
+    display_triangle()
     pygame.display.update()
 
 def char_movement(keys_pressed, char_game):
@@ -48,14 +48,16 @@ def char_movement(keys_pressed, char_game):
         if keys_pressed[pygame.K_w]:
             char_game.y -= velocity
 
-def triangle_hitbox():
-    #draws a triangle hitbox
-    pygame.draw.polygon((window), (255, 255, 0), [(50,50), (200,70), (200,25)])
+        if pygame.Rect.colliderect(char_game):
+            print("Crash!")
 
 def main():
     #starting position, (x, y), (w, h)
     char_game = pygame.Rect((char_x, char_y), (char_w, char_h))
     nme_game = pygame.Rect((nme_pos), (nme_w, nme_h))
+
+    if pygame.Rect.colliderect(wall):
+        print("Crash!")
 
     #game run
     clock = pygame.time.Clock()
@@ -85,7 +87,7 @@ def main():
         if char_game.y <= 748:
             char_game.y ==  748
 
-        display_window(char_game, nme_game, triangle)
+        display_window(char_game, nme_game)
 
 pygame.QUIT
 
